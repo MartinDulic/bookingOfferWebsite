56731c23e9d9f5c7fae7f63ebe85b62ab55d0d5c
MartinDulic <dulicmartin@gmail.com>
2026-02-14 14:37:00 +0100

    fixed /hr/kontakt to include text above the form

--- src/sections/ContactSection.jsx
+++ src/sections/ContactSection.jsx
@@ -8,12 +8,12 @@ import React from 'react'
 const ContactSection = () => {
   return (
     <Section className={"min-h-dvh bg-neutral-100 flex flex-col items-center"}>
-      {/* <Title className={"text-center text-primary-600"}>Kontaktirajte nas</Title>
+      <Title className={"text-center text-primary-600"}>Kontaktirajte nas</Title>
       <BodyText className={"my-8 text-neutral-800 w-full max-w-lg"}>
         <div className='mb-4 italic'>Nakon što ispunite obrazac, kontaktirat ćemo vas u najkraćem mogućem roku.</div>
         <div className='mb-4 italic'>Također možete nas nazvati na broj: <a href='tel:+385992032607' className='font-bold text-primary underline'>+385 99 203 2607</a></div>
       </BodyText>
-      <GoogleSheetsLeadForm 
+      {/* <GoogleSheetsLeadForm 
         className={"bg-white shadow-md w-full max-w-lg"}
         invalidPhoneText={"Neispravan broj telefona"}
         invalidEmailText={"Neispravan email"}
